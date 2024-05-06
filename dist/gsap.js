a64768faea9cd9c0dbc94ae697b65802964d52e1
ShivamThakurOfiicial40 <154581410+ShivamThakurOfiicial40@users.noreply.github.com>
2024-05-06 17:11:53 +0530

    gsap installation

---
+++ dist/gsap.js
@@ -0,0 +1,17 @@
+
+
+
+gsap.from("button,img,a",{
+    x: 223,
+    opacity: 0,
+    stagger: 0.1,
+    duration: 1
+    
+})
+gsap.from("#heading h2,h1,p",{
+    y: -30,
+    opacity: 0,
+    stagger: 0.5,
+    duration: 1
+    
+})

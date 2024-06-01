6bec5d56ecde67ae92c6b1ed1a337b2894226216
ShivamThakurOfiicial40 <154581410+ShivamThakurOfiicial40@users.noreply.github.com>
2024-06-01 10:46:37 +0530

    contact dailog section add

--- script.js
+++ script.js
@@ -66,3 +66,21 @@ const callBtn = document.querySelector('.contact');
 callBtn.addEventListener("click", () =>{
   ContactContainer.classList.toggle('hidden');
 })
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const openModalButton = document.getElementById('openModalButton');
+  const closeModalButton = document.getElementById('closeModalButton');
+  const modal = document.getElementById('modal');
+
+  openModalButton.addEventListener('click', () => {
+    modal.classList.remove('opacity-0', 'pointer-events-none');
+    modal.classList.add('opacity-100');
+  });
+
+  closeModalButton.addEventListener('click', () => {
+    modal.classList.remove('opacity-100');
+    modal.classList.add('opacity-0', 'pointer-events-none');
+  });
+});
